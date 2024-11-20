30bdda7e22fb4488228cca6f76590766d4d9a3de
Himani0002 <himanigohil0002@gmail.com>
2024-11-20 20:47:03 +0530

    HG | 20/11/24 | DAY1

--- index.js
+++ index.js
@@ -1,12 +1,46 @@
-// // const apple = require("./apple");
-// // const banana = require("./banana");
-// // const mango = require("./mango");
-// // // Fruits.js
-// // const fruits = ["apple", "banana", "cherry"];
-// // module.exports = { fruits };'
-// import { sum } from "./Math.js";
-// import { generate, count } from "random-words";
-// // Use the imported values
-// console.log(sum(2, 3)); // Example usage
-// console.log(generate());
+import express from "express";
+import net from "net";
 
+const app = express();
+const DEFAULT_PORT = 3000;
+
+// Middleware for logging
+app.use((req, res, next) => {
+  console.log(`${req.method} request to ${req.url}`);
+  next();
+});
+
+// Define routes
+app.get("/", (req, res) => {
+  res.send("Home route connected");
+});
+
+app.get("/apple", (req, res) => {
+  res.send("Apple connected");
+});
+
+app.get("/banana", (req, res) => {
+  res.send("Banana connected");
+});
+
+// Catch-all route for unmatched paths
+app.get("*", (req, res) => {
+  res.status(404).send("<h1>404 Not Found</h1>");
+});
+
+// Function to find a free port
+function findFreePort(startPort, callback) {
+  const server = net.createServer();
+  server.listen(startPort, () => {
+    server.once("close", () => callback(startPort));
+    server.close();
+  });
+  server.on("error", () => findFreePort(startPort + 1, callback));
+}
+
+// Start server on a free port
+findFreePort(DEFAULT_PORT, (freePort) => {
+  app.listen(freePort, () => {
+    console.log(`Server is running at http://localhost:${freePort}`);
+  });
+});
